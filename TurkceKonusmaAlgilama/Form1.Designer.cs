c2f0a6d331fdc119cb452d314d790acab04fc405
Furkan <falniak95@gmail.com>
2018-09-04 21:05:31 +0300

    Proje eklendi.

--- TurkceKonusmaAlgilama/Form1.Designer.cs
+++ TurkceKonusmaAlgilama/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(16, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ses Kayıt Et";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(278, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kaydı Durdur Ve Çöz";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(16, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(504, 189);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Algılanan Metin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 363);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TurkceKonusmaAlgilama/Form1.cs
+++ TurkceKonusmaAlgilama/Form1.cs
@@ -7,14 +7,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//---------------------------
+#region Gerekli Kütüphaneler
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Auth;
+using Google.Apis.Services;
+using System.Net;
+using System.Media;
+using System.Diagnostics;
+using NAudio.Wave;
+using System.IO;
+#endregion
+//---------------------------
 
 namespace TurkceKonusmaAlgilama
 {
     public partial class Form1 : Form
     {
+        private BufferedWaveProvider bwp;
+        WaveIn waveIn;
+        WaveOut waveOut;
+        WaveFileWriter writer;
+        WaveFileReader reader;
+        string output = "audio.raw";
+
         public Form1()
         {
             InitializeComponent();
+
+            waveOut = new WaveOut();
+            waveIn = new WaveIn();
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+            bwp = new BufferedWaveProvider(waveIn.WaveFormat);
+            bwp.DiscardOnBufferOverflow = true;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                    if (NAudio.Wave.WaveIn.DeviceCount < 1)
+            {
+                MessageBox.Show("Etkin bir mikrofon bulamadım.", "Mikrofon Bağlı Değil", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (NAudio.Wave.WaveIn.DeviceCount < 1)
+            {
+                MessageBox.Show("Etkin bir mikrofon bulamadım.", "Mikrofon Bağlı Değil", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            button1.Enabled = false;
+            button2.Enabled = true;
+            waveIn.StartRecording();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            waveIn.StopRecording();
+
+            if (File.Exists("audio.raw"))
+                File.Delete("audio.raw");
+
+
+            writer = new WaveFileWriter(output, waveIn.WaveFormat);
+
+
+
+            byte[] buffer = new byte[bwp.BufferLength];
+            int offset = 0;
+            int count = bwp.BufferLength;
+
+            var read = bwp.Read(buffer, offset, count);
+            if (count > 0)
+            {
+                writer.Write(buffer, offset, read);
+            }
+
+            waveIn.Dispose();
+            waveIn = null;
+            writer.Close();
+            writer = null;
+
+            reader = new WaveFileReader("audio.raw"); // (new MemoryStream(bytes));
+            waveOut.Init(reader);
+            waveOut.PlaybackStopped += new EventHandler<StoppedEventArgs>(waveOut_PlaybackStopped);
+            //    waveOut.Play();
+
+            reader.Close();
+
+            if (File.Exists("audio.raw"))
+            {
+
+                var speech = SpeechClient.Create();
+
+                var response = speech.Recognize(new RecognitionConfig()
+                {
+                    Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                    SampleRateHertz = 16000,
+                    LanguageCode = "tr",
+
+                }, RecognitionAudio.FromFile("audio.raw"));
+
+                // AIzaSyCMVUZen9fupUmN2QOo1fIIvnCjoPPmEUY 
+
+                textBox1.Text = "";
+
+                foreach (var result in response.Results)
+                {
+                    foreach (var alternative in result.Alternatives)
+                    {
+                        textBox1.Text = textBox1.Text + " " + alternative.Transcript;
+                    }
+                }
+                MessageBox.Show("Tamamlandı. Algılanan: " + textBox1.Text);
+                if (textBox1.Text.Length == 0)
+                    textBox1.Text = "Ses kaydı çok uzun ya da hiç ses algılanamadı.";
+            }
+            else
+            {
+
+                textBox1.Text = "Ses Dosyası Bulunamadı";
+
+            }
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            this.Cursor = Cursors.Default;
+            waveIn = new WaveIn();
+
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+        }
+
+        private void waveOut_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            try
+            {
+                waveOut.Stop();
+                reader.Close();
+                reader = null;
+            }
+            catch
+            {
+
+            }
+        }
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+
+        }
+
     }
 }
